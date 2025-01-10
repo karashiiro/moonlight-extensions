b9e7a9bc04d537f285237fe7113b3ec5ccb2789f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-01-10 07:08:29 -0800

    Add time options

--- src/remind-me/webpackModules/entrypoint.tsx
+++ src/remind-me/webpackModules/entrypoint.tsx
@@ -10,8 +10,15 @@ logger.info("someLibrary exports:", someLibrary);
 const natives = moonlight.getNatives("remind-me");
 logger.info("node exports:", natives);
 
-// hack because types are broken
-const RemindMeMenuItem = (<MenuItem id="remind-me" label="Remind Me" />) as unknown as MenuElement;
+const RemindMeMenuItem = (
+  <MenuItem id="remind-me" label="Remind Me">
+    <MenuItem id="remind-me-30m" label="In 30 Minutes" />
+    <MenuItem id="remind-me-1h" label="In 1 Hour" />
+    <MenuItem id="remind-me-3h" label="In 3 Hours" />
+    <MenuItem id="remind-me-24h" label="In 24 Hours" />
+    <MenuItem id="remind-me-custom" label="Custom..." />
+  </MenuItem>
+) as unknown as MenuElement; // hack because types are broken
 
 logger.info("Adding Remind Me to message context menu");
 
