1b9174a0db49cfaf0a48a86bef28dfa94e460a1f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-01-10 07:11:04 -0800

    Menu adjustments

--- src/remind-me/webpackModules/entrypoint.tsx
+++ src/remind-me/webpackModules/entrypoint.tsx
@@ -1,5 +1,5 @@
 import someLibrary from "@moonlight-mod/wp/remind-me_someLibrary";
-import ContextMenu, { MenuItem } from "@moonlight-mod/wp/contextMenu_contextMenu";
+import ContextMenu, { MenuItem, MenuSeparator } from "@moonlight-mod/wp/contextMenu_contextMenu";
 import React from "@moonlight-mod/wp/react";
 import { MenuElement } from "@moonlight-mod/types/coreExtensions/contextMenu";
 
@@ -12,11 +12,12 @@ logger.info("node exports:", natives);
 
 const RemindMeMenuItem = (
   <MenuItem id="remind-me" label="Remind Me">
-    <MenuItem id="remind-me-30m" label="In 30 Minutes" />
-    <MenuItem id="remind-me-1h" label="In 1 Hour" />
-    <MenuItem id="remind-me-3h" label="In 3 Hours" />
-    <MenuItem id="remind-me-24h" label="In 24 Hours" />
-    <MenuItem id="remind-me-custom" label="Custom..." />
+    <MenuItem id="remind-me-30m" label="In 30 Minutes" action={() => {}} />
+    <MenuItem id="remind-me-1h" label="In 1 Hour" action={() => {}} />
+    <MenuItem id="remind-me-3h" label="In 3 Hours" action={() => {}} />
+    <MenuItem id="remind-me-24h" label="In 24 Hours" action={() => {}} />
+    <MenuSeparator />
+    <MenuItem id="remind-me-custom" label="Custom..." action={() => {}} />
   </MenuItem>
 ) as unknown as MenuElement; // hack because types are broken
 
