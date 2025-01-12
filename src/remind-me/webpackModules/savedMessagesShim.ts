91e979c90f3a984e0771aa93f0c9a1955d469a6a
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-01-12 09:12:40 -0800

    Add more notes on saved messages API

--- src/remind-me/webpackModules/savedMessagesShim.ts
+++ src/remind-me/webpackModules/savedMessagesShim.ts
@@ -10,19 +10,26 @@ logger.info("Loaded saved messages shim");
 
 logger.info(SavedMessagesStore);
 
-export function putSavedMessage(message: SavedMessage) {
+export function putSavedMessage(message: SavedMessage): Promise<void> {
+  // Save message metadata to external resource (no Flux interaction)
   logger.info("Saving message");
   logger.info(message);
   return Promise.resolve();
 }
 
-export function deleteSavedMessage(message: SavedMessage) {
+export function deleteSavedMessage(message: SavedMessage): Promise<boolean> {
+  // Delete message metadata from external resource (no Flux interaction)
   logger.info("Deleting saved message");
   logger.info(message);
   return Promise.resolve(true);
 }
 
-export function getSavedMessages() {
+export function getSavedMessages(): Promise<void> {
+  /*
+  Fetch full messages and dispatch a Flux store event like this:
+
+  { type: "SAVED_MESSAGES_UPDATE", savedMessages: [{ message: { ... }, saveData: { ??? } }] }
+  */
   logger.info("Updating saved messages");
   return Promise.resolve();
 }
